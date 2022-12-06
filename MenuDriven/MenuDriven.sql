01348799548f6e643f982cbb929fa80b850f3090
animesh2cool <63170497+animesh2cool@users.noreply.github.com>
2022-12-06 18:36:57 +0530

    to store data in menudriven

---
+++ MenuDriven/MenuDriven.sql
@@ -0,0 +1,3 @@
+use anudip;
+create table student_mange (sid int(20), sname varchar (30), sphone numeric (10), sadd varchar (50));
+create table menudriven (eid int(20), ename varchar (30), eage varchar(10), ecity varchar (20), esalary int (10));
