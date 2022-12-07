a2b291fa806b1d2c9a7605733b6ba68763844d35
animesh2cool <63170497+animesh2cool@users.noreply.github.com>
2022-12-07 16:03:28 +0530

    Delete MenuDriven.sql

--- MenuDriven/MenuDriven.sql
+++
@@ -1,3 +0,0 @@
-use anudip;
-create table student_mange (sid int(20), sname varchar (30), sphone numeric (10), sadd varchar (50));
-create table menudriven (eid int(20), ename varchar (30), eage varchar(10), ecity varchar (20), esalary int (10));
